82bd7f557e84fe7f76afd7e54d771eefe8fa8751
AniBalyan <AniBalyan@LAPTOP-LCE2EEK2>
2023-11-26 23:43:39 +0400

    Add project files.

---
+++ Lesson/Program.cs
@@ -0,0 +1,22 @@
+﻿using Lesson;
+
+class Program
+{
+    static void Main()
+    {
+
+        passengersDetails passenger1 = new passengersDetails("John", "New York", 17, 1000);
+     
+        Console.WriteLine("Name: " + passenger1.getName());
+        Console.WriteLine("Destination: " + passenger1.getDestination());
+        Console.WriteLine("Passenger Age: " + passenger1.getPassengerAge());
+        Console.WriteLine("Ticket Price: " + passenger1.getTicketPrice());
+
+        passengersDetails passenger2 = new passengersDetails ("Steve", "Moscow", 20, 1000);
+
+        Console.WriteLine("Name: " + passenger2.getName());
+        Console.WriteLine("Destination: " + passenger2.getDestination());
+        Console.WriteLine("Passenger Age: " + passenger2.getPassengerAge());
+        Console.WriteLine("Ticket Price: " + passenger2.getTicketPrice());
+    }
+}
---
+++ Lesson/passengersDetails.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson
+{
+    internal class passengersDetails
+    {
+        private String passengerName;
+        private String destination;
+        private byte passengerAge;
+        private int ticketPrice;
+        private Boolean isMinor;
+
+         public passengersDetails(String name, String dest, byte age, int price)
+        {
+            passengerName = name;
+            destination = dest;
+            passengerAge = age;
+            isMinor = (passengerAge < 18);
+            ticketPrice = price;
+        }
+
+        public int getTicketPrice()
+        {
+            if (isMinor)
+            {
+                ticketPrice = ticketPrice - ticketPrice*10/100;
+
+            }
+            return ticketPrice;
+        }
+
+        public void setTicketPrice(int ticketPrice)
+        {
+            this.ticketPrice = ticketPrice;
+        }
+
+        public String getName() { 
+            return passengerName; 
+        }
+
+        public void setName(String name)
+        {
+            passengerName = name;
+        }
+
+        public String getDestination()
+        {
+            return destination;
+        }
+
+        public void setDestination(String destination)
+        {
+            this.destination = destination;
+        }
+
+        public byte getPassengerAge()
+        {
+            return passengerAge;
+        }
+
+        public void setPassengerAge(byte passengerAge)
+        {
+            this.passengerAge = passengerAge;
+        }
+
+      
+
+    }
+}
